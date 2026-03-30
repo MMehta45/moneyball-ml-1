dc7a09b6c6e97872656e26a8de89fc3f5d969fc2
Niket Sheth <niketsheth31@gmail.com>
2026-03-30 10:00:01 -0500

    added code to graph the nodes and edges using matplotlib

--- create_graph.py
+++ create_graph.py
@@ -1,4 +1,5 @@
 import networkx as nx
+import matplotlib.pyplot as plt
 
 dummy_data = {
     "CS1336": {"name": "Intro to CS", "prereqs": [], "difficulty": 0.5},
@@ -6,6 +7,7 @@ dummy_data = {
     "CS2336": {"name": "Computer Science II", "prereqs": ["CS1337"], "difficulty": 0.9},
     "ARTS1301": {"name": "Art Appreciation", "prereqs": [], "difficulty": 0.2}
 }
+
 def create_graph(data):
     degree_flow = nx.DiGraph()
     for course in data:
@@ -15,4 +17,16 @@ def create_graph(data):
            degree_flow.add_edge(prereq_course, course)
     return degree_flow
            
+
+sample_graph = create_graph(dummy_data)
+
+position = nx.spring_layout(sample_graph)
+nx.draw_networkx_nodes(sample_graph, position, node_color='lightblue', node_size=2000)
+nx.draw_networkx_edges(sample_graph, position, arrowstyle='->', arrowsize=20)
+nx.draw_networkx_labels(sample_graph, position, font_size=10, font_weight='bold')
+
+plt.title("Degree Plan Graph")
+plt.axis('off')
+plt.show()
+
 
