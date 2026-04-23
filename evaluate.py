e84bc8e2bc7c367fdc9795b2c835438870a3f117
Niket Sheth <niketsheth31@gmail.com>
2026-04-22 19:01:44 -0500

    added code to evalute for tracker and skeleton eval (not finished yet)

--- evaluate.py
+++ evaluate.py
@@ -1,3 +1,84 @@
-# this file will contain code for tracker and skeleton eval.
 
-print("Hello")
+DEATH_PENALTY = -10000000
+
+class Tracker:
+    def __init__(self, requirements):
+        self.history = set()
+        self.total_hours = 0
+        self.core_totals = {key: 0 for key in requirements}
+        self.semester_difficulty = 0
+
+def update_core_totals(course_id, tracker, requirements, graph):
+    for core_category, info in requirements.items():
+        if course_id in info["courses"]:
+            tracker.core_totals[core_category] += graph.nodes[course_id]["credit_hours"]
+
+
+def evaluate(individual, G, requirements):
+    #history = set()
+    #core_totals = {key: 0 for key in requirements}
+    tracker = Tracker(requirements)
+
+    # Looping through all the semesters of the entire degree plan
+    for i, semester in enumerate(individual):
+        sem_hours = 0
+        sem_difficulty = 0
+        sem_course_counter = 0
+
+        # Looping courses in a semester
+        for course_id in semester:
+
+            # checking if course is already in history
+            if course_id in tracker.history:
+                return DEATH_PENALTY
+
+            data = G.nodes[course_id]
+
+            # 1. Prerequisite check
+            prereqs = data["prereqs"]
+            for prereq in prereqs:
+                if prereq not in tracker.history:
+                    return DEATH_PENALTY
+                
+            # 2. Availability check
+            term = "Fall" if i % 2 == 0 else "Spring"
+            if term not in data["availability"]:
+                return DEATH_PENALTY
+            
+            # Update trackers in the loop
+            sem_hours += data["credit_hours"]
+            sem_difficulty += data["difficulty"]
+            sem_course_counter += 1
+
+            # access the tracker class and accounts for requirements.json
+            update_core_totals(course_id, tracker, requirements, G)
+
+        # 3. Semester Load/Credit Hours (max 19)
+        if sem_hours > 19:
+            return DEATH_PENALTY
+        
+        # Update the tracker class
+        tracker.total_hours += sem_hours
+        tracker.history.update(semester)
+        tracker.semester_difficulty += round(sem_difficulty / sem_course_counter, 2) if sem_course_counter > 0 else 0  
+
+    
+    # Checking for total hours requirement (120)
+    if tracker.total_hours < 120:
+        return DEATH_PENALTY
+    
+    # Checking Degree Audit
+    for core_category, info in requirements.items():
+        if tracker.core_totals[core_category] < info["hours_required"]:
+            return DEATH_PENALTY
+        
+
+    # 
+    return (0,) # placeholder value
+
+        
+
+
+
+
+
