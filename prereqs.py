026ad39e46d1fe8ddbdd01e230cd836fd340c711
izmakhu <143165806+izmakhu@users.noreply.github.com>
2026-04-23 21:16:23 -0500

    updated schedule format

--- prereqs.py
+++ prereqs.py
@@ -3,24 +3,41 @@ import json
 with open("data_updated2.json", "r") as f:
     data = json.load(f)
 
-schedule = ["CS1436", "CS1337"]
+#mock schedule    
+schedule = [["CS1436", "MATH2413", "ECS1100"], 
+            ["CS1337", "MATH2414", "PHYS2325/2125", "CS2305"],
+            ["CS2340", "PHYS2326/2126", "MATH2418"],
+            ["ECS2390", "CS3341", "CS3345", "CS3377"],
+            ["CS4337", "CS4341/4141", "CS3354"],
+            ["CS4349", "CS3162","CS4348"],
+            ["CS4384", "CS4347"],
+            ["CS4485"]]
 
 score = 0
-
 prereq_count = {}
 
 for courses in data:
     for prereq in data[courses]["prereqs"]:
         prereq_count[prereq] = 1 + prereq_count.get(prereq, 0)
-
-#for schedule in shcedules:
-for i, course in enumerate(schedule):
-    #sequential proximity scoring
-    if i != 0 and schedule[i-1] in data[course]["prereqs"]:
-        score += 4
     
-    #prereq points scoring
-    score += prereq_count[course] * 2
+#with open("output.txt", "w") as f:
+    #for prereq, count in prereq_count.items():
+        #print(f"{prereq}: {count}", file=f)
 
+#for schedule in schedules:
+for i, semester in enumerate(schedule):
+    for course in semester:
+        #sequential proximity scoring
+        #are any of the prereqs taken in the semester before
+        if i != 0:
+            prev_sem = set(schedule[i-1])
+            prereqs = set(data[course]["prereqs"])
+            matches = len(prev_sem and prereqs)
+            score += matches * 4
+        
+        #prereq points scoring
+        if course in prereq_count:
+            score += prereq_count[course] * 2
 print(score)
+
 
--- seniorload.py
+++ seniorload.py
@@ -3,18 +3,26 @@ import json
 with open("data_updated2.json", "r") as f:
     data = json.load(f)
 
-schedule = ["null", "null", "null", "null", "null", "null", "MATH2413", "CS4398"]
+schedule = [["CS1436", "MATH2413", "ECS1100"], 
+            ["CS1337", "MATH2414", "PHYS2325/2125", "CS2305"],
+            ["CS2340", "PHYS2326/2126", "MATH2418"],
+            ["ECS2390", "CS3341", "CS3345", "CS3377"],
+            ["CS4337", "CS4341/4141", "CS3354"],
+            ["CS4349", "CS3162","CS4348"],
+            ["CS4384", "CS4347"],
+            ["CS4334"]]
 
 score = 0
 
 #assuming indexing starts at 0
-#if it is less than an A-
 
-if data[schedule[6]]["difficulty_Score"] > 0.33:
-    #this is weighted solution but can also just subtract 8 if hard
-    score -= data[schedule[6]]["difficulty_Score"] * 8
-if data[schedule[7]]["difficulty_Score"] > 0.33:
-    score -= data[schedule[7]]["difficulty_Score"] * 8
+for course in schedule[6]:
+    if data[course]["difficulty_Score"] > 0.85:
+        #this is weighted solution but can also just subtract 8 if hard
+        score += round(data[course]["difficulty_Score"] * 8)
+for course in schedule[7]:
+    if data[course]["difficulty_Score"] > 0.85:
+        score += round(data[course]["difficulty_Score"] * 8)
 
 print(score)
     
